a26b7524c1bc265bc6d1f11daf6a096cf16df724
bijjr <bjffrys@gmail.com>
2016-01-07 23:50:31 -0500

    Multiply Characters

---
+++ multiplychar.py
@@ -0,0 +1,18 @@
+# Here we have a function that help us spam our hearty laughter. But is not working! I need you to find out why...
+
+# Expected results:
+
+# spam(1);#hue
+# spam(6);#huehuehuehuehuehue
+# spam(14);#huehuehuehuehuehuehuehuehuehuehuehuehuehue
+
+fix this code!
+def spam(number):
+    return ['hue' for i in range(number+1)]
+
+ #Solution
+
+def spam(number):
+	return 'hue' * number
+
+#This made me feel stupid..
