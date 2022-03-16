6f5b36c6b9c604c7c3927ce9328cce1426bacd62
leohavethmercy <billiejefferys@gmail.com>
2022-03-16 00:36:28 -0400

    Updating Solution

--- curryingfunctions.js
+++ curryingfunctions.js
@@ -1,12 +1,19 @@
-let multiplyAll = array => number => {
-    let arrayTwo = []
-    for (let i = 0; i < array.length; i++) {
-      let add = 0
-      add = array[i] * number   
-      arrayTwo.push(add)     
-    }
-      return arrayTwo
-  }
+// let multiplyAll = array => number => {
+//     let arrayTwo = []
+//     for (let i = 0; i < array.length; i++) { 
+//       arrayTwo.push(array[i] * number )     
+//     }
+//     return arrayTwo
+// }
 
+
+//  console.log(multiplyAll([1, 2, 3])(2))
+
+ // After looking at solutions realized that .map()
+ // is the cleaner, efficient solution
+
+ let multiplyAll = (array) => (numbers) => {
+     return array.map(x => x * numbers)
+ }
 
  console.log(multiplyAll([1, 2, 3])(2))
